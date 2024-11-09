af79bf1919a5c5d662c7a8a64d571913f2bf5dec
GrishaChem <Hryhoryy.chemerys@gmail.com>
2024-11-09 23:14:37 +0200

    qweasdzxc

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,17 @@
-const generateContacts = async (number) => {};
+import createFakeContact from "../utils/createFakeContact.js"
+import readContacts from "../utils/readContacts.js";
+import writeContacts from "../utils/writeContacts.js"
 
-generateContacts(5);
+const generateContacts = async (number) => {
+
+    for (let i = 0; i < number; i++) {
+        const data = createFakeContact()
+        console.log("Generated contact:", data);
+        const contacts = await readContacts()
+        await writeContacts([...contacts, data])
+    }
+
+};
+
+generateContacts(5).catch(err => console.error(err));
+
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from "node:fs/promises"
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+    const data = await fs.readFile(PATH_DB, { encoding: "utf-8" })
+    
+    if (data.length < 1)
+        {return []}
+    const contacts = JSON.parse(data);
+    return contacts;
+};
+readContacts().catch(err => console.error(err))
+
+export default (readContacts)
