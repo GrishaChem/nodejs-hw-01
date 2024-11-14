a827594980deda0505ddfc52346aa9af60033102
GrishaChem <Hryhoryy.chemerys@gmail.com>
2024-11-14 21:39:34 +0200

    qweasdzxc

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -4,7 +4,9 @@ import writeContacts from "../utils/writeContacts.js"
 export const removeLastContact = async () => {
 
     const data = await readContacts();
+    // console.log("data-2: ", data);
     const newData = data.slice(0, -1);
+    // console.log("data-3: ",newData);
     writeContacts(newData);
 
  };
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -3,12 +3,12 @@ import * as fs from "node:fs/promises"
 
 export const readContacts = async () => {
     const data = await fs.readFile(PATH_DB, { encoding: "utf-8" })
-    
+    // console.log("data-1: ", data);
     if (data.length < 1)
         {return []}
     const contacts = JSON.parse(data);
+    // console.log("data-1.1: ",contacts);
     return contacts;
 };
-readContacts().catch(err => console.error(err))
 
 export default (readContacts)
